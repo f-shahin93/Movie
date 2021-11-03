41f1f9d3fb1dce55168f25af4ca9c123b2c98c65
F_Shahin <f.shahin.kh@gmail.com>
2021-11-04 02:00:14 +0330

    Create buildSrc for version and name implementations.

---
+++ buildSrc/src/main/kotlin/com/shahin/movieapp/Android.kt
@@ -0,0 +1,10 @@
+package com.shahin.movieapp
+
+object Android {
+    const val minSdkVersion = 21
+    const val targetSdkVersion = 30
+    const val compileSdkVersion = 30
+    const val applicationId = "com.shahin.movieapp"
+    const val versionCode = 1
+    const val versionName = "1.0"
+}
